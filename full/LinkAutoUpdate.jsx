c1f9bbacfac35f46e1f215ddf4a3d18991896b45
Marc Autret <marc@indiscripts.com>
2021-02-15 12:11:05 +0100

    LinkAutoUpdate | Automatically refresh links in InDesign [210215]

---
+++ full/LinkAutoUpdate.jsx
@@ -0,0 +1,112 @@
+/*******************************************************************************
+
+		Name:           LinkAutoUpdate [4.1]
+		Desc:           Automatically updates 'out-of-date' links.
+		Path:           /snip/LinkAutoUpdate.jsx
+		Encoding:       ÛȚF8
+		Compatibility:  InDesign CS5/CS6/CC [Mac/Win]
+		L10N:           ---
+		Kind:           Script / Startup script
+		API:            ---
+		DOM-access:     YES
+		Todo:           ---
+		Created:        181604 (YYMMDD)
+		Modified:       210215 (YYMMDD)
+
+*******************************************************************************/
+
+	//==========================================================================
+	// PURPOSE
+	//==========================================================================
+	
+	/*
+
+	This script automatically updates 'out-of-date' links in InDesign. It is a
+	listener. You can either run it manually during your session, or load it
+	as a 'startup script'.
+	
+	A alternate code exists based on a script from あるふぁ（仮） :
+	twitter.com/peprintenpa/status/1361132929678667787?ref_src=twsrc%5Etfw
+	which listens to the event `afterAttributeChanged` and delegates the task
+	to BridgeTalk.
+	
+	The present implementation relies on the event `afterLinksChanged`
+	available in CS5/CS6/CC. Its target is the document itself, so we access
+	the links from `ev.target.links.everyItem()`. (For the record, the
+	alternate implementation is kept in the event handler.)
+
+	*/
+
+	#targetengine 'LinkAutoUpdate'
+	
+	'string' == typeof $.global.EVENT_TYPE
+	|| ( $.global.EVENT_TYPE = 'afterLinksChanged'); // Alt. 'afterAttributeChanged'
+
+
+	'function' == typeof $.global.onEvent || ($.global.onEvent= function onEvent(/*Event*/ev,  tg)
+	//----------------------------------------------------------
+	// ev.eventType :: 'afterLinksChanged'  |  'afterAttributeChanged'
+	// ev.target    :: Document             |  any
+	{
+		if( !(tg=(ev||0).target) ) return;
+		
+		if( 'afterLinksChanged' == ev.eventType )
+		{
+			try{ (tg instanceof Document) && (tg=tg.links.everyItem()).isValid && tg.update() }
+			catch(e){ alert($.engineName + " Error:\r" + e) }
+			return;
+		}
+
+		// Alternate code.
+		// ---
+		if( 'afterAttributeChanged' == ev.eventType )
+		{
+			if( 'Link' != tg.constructor.name || tg.status != LinkStatus.LINK_OUT_OF_DATE ) return;
+
+			// ---
+			// We cannot simply invoke `tg.update()` from within the event handler
+			// as InDesign would consider it 'removing' the event target and fire a
+			// runtime error. The solution is to delegate the task to BridgeTalk.
+			// ---
+
+			with( new BridgeTalk )
+			{
+				target = BridgeTalk.appSpecifier + '#' + $.engineName;
+				body = $.global.localize('try{resolve(%1).update()}catch(e){alert("%2 Error:\r"+e)}'
+					, tg.toSpecifier().toSource()
+					, $.engineName
+					);
+				send();
+			}
+		}
+
+	})
+	.help = $.global.localize("%1\r\r%2\r\r%1"
+	, '-----------------------'
+	, [
+	    "> Out-of-date links will refresh automatically.",
+	    "> Re-run this script to turn it off.",
+	    "[Tip] Put the JSX file in your `startup scripts` folder to make it automatically active."
+	  ].join('\r\r')
+	);
+
+	(function(/*str*/evType,/*str*/name,/*fct*/callback,  evls,t)
+	//----------------------------------------------------------
+	// Install/uninstall the listener.
+	{
+		evls = app.eventListeners;
+		if( (t=evls.itemByName(name)).isValid && t.properties.handler===callback )
+		{
+			t.remove();
+			alert(name + " is turned OFF.");
+		}
+		else
+		{
+			// Prevent alert on automatic startup.
+			// ---
+			t = app.performanceMetric(+PerformanceMetricOptions.MINISAVE_COUNT);
+			if( t ) alert(name + " is turned ON.\r\r" + callback.help);
+			evls.add(evType,callback,void 0,{name:name});
+		}
+
+	})(EVENT_TYPE, $.engineName, onEvent);
